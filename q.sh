f0ad7b4447f3960499cca90c32307b353286efe8
Artem Kalmykov <kalmykov.artem@gmail.com>
2016-11-20 17:33:12 -0800

    colored output

--- q.sh
+++ q.sh
@@ -8,11 +8,11 @@ NAME=a.out
 
 # Files for compiling
 # add new CFILES="main.c second.c" or add new line CFILES+="third.c"
-CFILES=main.c
+CFILES=sort_list.c
 
 # Preset argument
 # ARG=file.txt or ARG="this is my test string"
-ARG="this is my test string"
+ARG=
 
 # Usefull flags
 WFLAGS="-Wall -Wextra -Werror"
@@ -70,9 +70,10 @@ then
 	echo -e "\twith 0 arguments, with multiple arguments, with preset (ARG variable) argument"
 	echo -e "\tand save history of your compilations and launches"
 	echo -e "\tdo not forget to configure...\n"
-	echo -e $WHT"q.sh$CLN arg1 arg2 arg3 arg4 arg5" "\t\tfor arguments"
+	echo -e $WHT"q.sh$CLN arg1 arg2 arg3 ..." "\t\tfor arguments"
 	echo -e $WHT"q.sh"$CLN "\t\t\t\t\tfor preset argument"
 	echo -e $WHT"q.sh -0"$CLN "\t\t\t\tfor NO arguments"
+	echo -e $WHT"q.sh -c"$CLN"\t\t\t\t\tfor colored compiler errors"
 	echo -e $WHT"q.sh -h"$CLN "\t\t\t\tor --help to show this help"
 	echo -e "\n"
 	exit 0
@@ -93,9 +94,15 @@ fi
 # Compiling 
 # add new Flags here
 LOG "$COMPILE"
-$COMPILE 2>&1 | tee -a $LOGS
+if [ "$1" == "-c" ]
+then
+	LOG "output color compiler errors"
+	$COMPILE
+else
+	$COMPILE 2>&1 | tee -a $LOGS
+fi
 
-# IF compiled, IF No arguments, IF 0 to 5 arguments, IF preset argument value
+# IF compiled, IF No arguments, IF 0 to 3... arguments, IF preset argument value
 if [ -a $NAME ]
 then
 	echo -e $BLU"compiled"$CLN
@@ -105,16 +112,16 @@ then
 		if [ "$1" == "-0" ]
 		then
 			echo -e $GRN"NO arguments"$CLN
-			LOG "running ${NAME} without arguments"
+			LOG "running ${NAME} without arguments\n"
 			./$NAME | tee -a $LOGS
 			exit 0
 		fi
-		LOG "running ${NAME} with $# arguments"
+		LOG "running ${NAME} with $# arguments: $@\n"
 		echo -e $GRN$# "argument(s):"$CLN $@
 		./$NAME $@ | tee -a $LOGS
 	else
 		echo -e $GRN"argument:"$CLN $ARG
-		LOG "running ${NAME} with preset argument"
+		LOG "running ${NAME} with 1 preset argument: $ARG\n"
 		./$NAME "$ARG" | tee -a $LOGS
 	fi
 else
